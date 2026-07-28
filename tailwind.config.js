8dc3cf76020eb0192ae396dc07dfdbabe6128f81
dnascimentto-cmyk <dnascimentto@gmail.com>
2026-07-28 04:31:01 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0B1F3A',
+        amber: '#C9A227',
+      },
+    },
+  },
+  plugins: [],
+};
